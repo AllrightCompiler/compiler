836e0c840e5bac92b4bdd64ed29df1139d375215
zhang-qh19 <zhang-qh19@mails.tsinghua.edu.cn>
2022-07-08 16:30:27 +0800

    config medium passes with file

--- src/mediumend/optmizer.hpp
+++ src/mediumend/optmizer.hpp
@@ -2,6 +2,8 @@
 
 #include "common/ir.hpp"
 #include "mediumend/cfg.hpp"
+#include <iostream>
+#include <fstream>
 
 namespace mediumend {
 
@@ -16,6 +18,11 @@ void main_global_var_to_local(ir::Program *prog);
 void gvn_gcm(ir::Program *prog);
 void function_inline(ir::Program *prog);
 
+// IMPORTANT: if add new pass, modify PASS_MAP in optmizer.cpp
+extern const std::map<std::string, std::function<void(ir::Program *)> > PASS_MAP;
+// define default passes in optmizer.cpp
+extern std::vector<std::function<void(ir::Program *)> > passes;
+
 ConstValue const_compute(ir::Instruction *inst, ConstValue oprand);
 ConstValue const_compute(ir::Instruction *inst, ConstValue op1, ConstValue op2);
 
@@ -24,28 +31,22 @@ inline void run_medium(ir::Program *prog) {
     func.second.cfg = new CFG(&func.second);
   }
   compute_use_def_list(prog);
-  main_global_var_to_local(prog);
-  mem2reg(prog);
 
-  remove_unused_function(prog);
-  main_global_var_to_local(prog);
+  std::ifstream pass_config("pass_config.txt");
+  if (pass_config.is_open()) {
+    std::cout << "use user pass config!" << std::endl;
+    passes.clear();
+    std::string line;
+    while (std::getline(pass_config, line)) {
+      if (!line.length()) continue; // empty line
+      assert(PASS_MAP.count(line));
+      passes.push_back(PASS_MAP.at(line));
+    }
+  }
 
-  // 纯函数可以用来做GVN和无用代码移除
-  mark_pure_func(prog);
-  remove_uneffective_inst(prog);
-
-  // function_inline(prog);
-  gvn_gcm(prog);
-  remove_uneffective_inst(prog);
-
-  // 下面这两步和SCCP感觉是等效的？
-  constant_propagation(prog);
-  simplification_phi(prog);
-
-  remove_uneffective_inst(prog);
-  // 移除无用指令后可能有的函数不会被调用，pure function / unreachable BB里的function
-  remove_unused_function(prog);
-  clean_useless_cf(prog);
+  for (auto pass : passes) {
+    pass(prog);
+  }
 }
 
 } // namespace mediumend
