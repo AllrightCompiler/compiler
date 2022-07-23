78dbf1ce8ab03f0d5afbe0a9d3c2d9120fc197ad
Kirin <1370302499@qq.com>
2022-07-23 16:02:36 +0800

    评测脚本增加与 GCC -O2 性能对比

--- runtime/sylib.c
+++ runtime/sylib.c
@@ -56,18 +56,18 @@ __attribute((constructor)) void before_main(){
   for(int i=0;i<_SYSY_N;i++)
     _sysy_h[i] = _sysy_m[i]= _sysy_s[i] = _sysy_us[i] =0;
   _sysy_idx=1;
-}  
+}
 __attribute((destructor)) void after_main(){
   for(int i=1;i<_sysy_idx;i++){
     fprintf(stderr,"Timer@%04d-%04d: %dH-%dM-%dS-%dus\n",\
       _sysy_l1[i],_sysy_l2[i],_sysy_h[i],_sysy_m[i],_sysy_s[i],_sysy_us[i]);
-    _sysy_us[0]+= _sysy_us[i]; 
+    _sysy_us[0]+= _sysy_us[i];
     _sysy_s[0] += _sysy_s[i]; _sysy_us[0] %= 1000000;
     _sysy_m[0] += _sysy_m[i]; _sysy_s[0] %= 60;
     _sysy_h[0] += _sysy_h[i]; _sysy_m[0] %= 60;
   }
   fprintf(stderr,"TOTAL: %dH-%dM-%dS-%dus\n",_sysy_h[0],_sysy_m[0],_sysy_s[0],_sysy_us[0]);
-}  
+}
 void _sysy_starttime(int lineno){
   _sysy_l1[_sysy_idx] = lineno;
   gettimeofday(&_sysy_start,NULL);
