8ac97bb95fb3cf000e192ecd955cfab7db47c857
ksitta <fruwen@163.com>
2022-08-14 00:12:30 +0800

    temp remove unroll

--- src/mediumend/optimizer.cpp
+++ src/mediumend/optimizer.cpp
@@ -70,7 +70,7 @@ std::vector<funcptr> passes = {
   main_global_var_to_local,
   mem2reg,
 
-  loop_unroll,
+  // loop_unroll,
 
   array_mem2reg,
     gvn_cfg,
@@ -81,7 +81,7 @@ std::vector<funcptr> passes = {
 
   gvn_cfg,
   gvn_cfg,
-  loop_unroll,
+  // loop_unroll,
 
   gvn_cfg,
   remove_useless_loop,
