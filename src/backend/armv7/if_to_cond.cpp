b24bfcbd9dddfe58c0e2b8e167e6d8bf92d2ae12
Kirin <1370302499@qq.com>
2022-08-19 20:45:35 +0800

    完善指令条件化

--- src/backend/armv7/if_to_cond.cpp
+++ src/backend/armv7/if_to_cond.cpp
@@ -36,6 +36,16 @@ conditionalize(BasicBlock const &bb, ExCond const cond) {
   return instrs;
 }
 
+std::optional<BasicBlock *> try_get_next(BasicBlock const &bb) {
+  if (bb.succ.empty()) {
+    return nullptr;
+  } else if (bb.succ.size() == 1u) {
+    return *bb.succ.begin();
+  } else {
+    return {};
+  }
+}
+
 void if_to_cond(Function &func) {
   for (auto &bb : func.bbs) {
     assert(!bb->insns.empty());
@@ -43,45 +53,61 @@ void if_to_cond(Function &func) {
     if (br == nullptr) {
       continue;
     }
-    if (br->true_target->insns.size() + br->false_target->insns.size() >
-        MAX_COND_INSTR_COUNT) {
+    auto const then_next = try_get_next(*br->true_target);
+    auto const otherwise_next = try_get_next(*br->false_target);
+    if (then_next && *then_next == br->false_target &&
+        br->true_target->insns.size() <= MAX_COND_INSTR_COUNT &&
+        can_be_cond(*br->true_target)) {
+      auto const cond = br->cond;
+      auto const true_target = br->true_target;
+      bb->insns.back() = std::move(br->cmp);
+      auto then_instrs = conditionalize(*true_target, cond);
+      bb->insns.insert(bb->insns.cend(),
+                       std::make_move_iterator(then_instrs.begin()),
+                       std::make_move_iterator(std::prev(then_instrs.end())));
+      BasicBlock::remove_edge(bb.get(), true_target);
+      bb->insns.push_back(std::make_unique<Branch>(*then_next));
+      BasicBlock::add_edge(bb.get(), *then_next);
+    } else if (otherwise_next && *otherwise_next == br->true_target &&
+               br->false_target->insns.size() <= MAX_COND_INSTR_COUNT &&
+               can_be_cond(*br->false_target)) {
+      auto const cond = br->cond;
+      auto const false_target = br->false_target;
+      bb->insns.back() = std::move(br->cmp);
+      auto otherwise_instrs = conditionalize(*false_target, logical_not(cond));
+      bb->insns.insert(
+          bb->insns.cend(), std::make_move_iterator(otherwise_instrs.begin()),
+          std::make_move_iterator(std::prev(otherwise_instrs.end())));
+      BasicBlock::remove_edge(bb.get(), false_target);
+      bb->insns.push_back(std::make_unique<Branch>(*otherwise_next));
+      BasicBlock::add_edge(bb.get(), *otherwise_next);
+    } else if (then_next && otherwise_next && *then_next == *otherwise_next &&
+               br->true_target->insns.size() + br->false_target->insns.size() <=
+                   MAX_COND_INSTR_COUNT &&
+               can_be_cond(*br->true_target) &&
+               can_be_cond(*br->false_target)) {
+      auto const cond = br->cond;
+      auto const true_target = br->true_target;
+      auto const false_target = br->false_target;
+      bb->insns.back() = std::move(br->cmp);
+      auto then_instrs = conditionalize(*true_target, cond);
+      bb->insns.insert(bb->insns.cend(),
+                       std::make_move_iterator(then_instrs.begin()),
+                       std::make_move_iterator(std::prev(
+                           then_instrs.end(), *then_next != nullptr)));
+      BasicBlock::remove_edge(bb.get(), true_target);
+      auto otherwise_instrs = conditionalize(*false_target, logical_not(cond));
+      bb->insns.insert(
+          bb->insns.cend(), std::make_move_iterator(otherwise_instrs.begin()),
+          std::make_move_iterator(
+              std::prev(otherwise_instrs.end(), *otherwise_next != nullptr)));
+      BasicBlock::remove_edge(bb.get(), false_target);
+      if (*then_next != nullptr) {
+        bb->insns.push_back(std::make_unique<Branch>(*then_next));
+        BasicBlock::add_edge(bb.get(), *then_next);
+      }
+    } else {
       continue;
-    }
-    if (br->true_target->succ.size() > 1u ||
-        br->false_target->succ.size() > 1u) {
-      continue;
-    }
-    auto const then_next = br->true_target->succ.empty()
-                               ? nullptr
-                               : *br->true_target->succ.begin();
-    auto const otherwise_next = br->false_target->succ.empty()
-                                    ? nullptr
-                                    : *br->false_target->succ.begin();
-    if (then_next != otherwise_next) {
-      continue;
-    }
-    if (!can_be_cond(*br->true_target) || !can_be_cond(*br->false_target)) {
-      continue;
-    }
-    auto const cond = br->cond;
-    auto const true_target = br->true_target;
-    auto const false_target = br->false_target;
-    bb->insns.back() = std::move(br->cmp);
-    auto then_instrs = conditionalize(*true_target, cond);
-    bb->insns.insert(bb->insns.cend(),
-                     std::make_move_iterator(then_instrs.begin()),
-                     std::make_move_iterator(
-                         std::prev(then_instrs.end(), then_next != nullptr)));
-    auto otherwise_instrs = conditionalize(*false_target, logical_not(cond));
-    bb->insns.insert(bb->insns.cend(),
-                     std::make_move_iterator(otherwise_instrs.begin()),
-                     std::make_move_iterator(std::prev(
-                         otherwise_instrs.end(), otherwise_next != nullptr)));
-    BasicBlock::remove_edge(bb.get(), true_target);
-    BasicBlock::remove_edge(bb.get(), false_target);
-    if (then_next != nullptr) {
-      bb->insns.push_back(std::make_unique<Branch>(then_next));
-      BasicBlock::add_edge(bb.get(), then_next);
     }
     debug(std::cerr) << "conditionalize " << bb->label << '\n';
   }
